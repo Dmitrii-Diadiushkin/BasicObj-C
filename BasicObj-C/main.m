e338ba487f09d96af2a4c1b4785e09159ce10f3f
Dmitrii Diadiushkin <stephandvd@yandex.ru>
2020-12-07 23:49:50 +0500

    End of lesson_1

--- BasicObj-C/main.m
+++ BasicObj-C/main.m
@@ -8,16 +8,26 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    
     int first = 0;
     int second = 0;
     printf("Enter first integer number: ");
     scanf("%d", &first);
     printf("Enter second integer number: ");
     scanf("%d", &second);
-    printf("%d + %d = %d", first, second, first + second);
+    printf("%d + %d = %d\n", first, second, first + second);
+    printf("%d - %d = %d\n", first, second, first - second);
+    printf("%d * %d = %d\n", first, second, first * second);
+    printf("%d / %d = %f\n", first, second, (double)first / (double)second);
+    int third = 0;
+    
+    printf("\nCalculating averege value of 3.\n");
+    printf("Enter first integer number: ");
+    scanf("%d", &first);
+    printf("Enter second integer number: ");
+    scanf("%d", &second);
+    printf("Enter third integer number: ");
+    scanf("%d", &third);
+    printf("The averege value of entered numbers is %f\n", ((double)first + (double)second + (double)third) / 3);
     return 0;
 }
