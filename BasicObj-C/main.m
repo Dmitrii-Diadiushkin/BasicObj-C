d5a9ca8d94ebb1100020ca33846907fb32b58712
Dmitrii Diadiushkin <stephandvd@yandex.ru>
2020-12-21 13:11:42 +0500

    lesson 4 complete

--- BasicObj-C/main.m
+++ BasicObj-C/main.m
@@ -6,8 +6,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Student.h"
 
 int main(int argc, const char * argv[]) {
-       
+    
+    Student *student1 = [[Student alloc] initWithfirstName: @"John" lastName:@"Silver" age:20];
+
+    NSLog(@"%d", student1.age);
+    
+    [student1 inceaseAge];
+    
+    NSLog(@"%@", student1.description);
+    
+    NSLog(@"%d", student1.age);
+    
     return 0;
 }
